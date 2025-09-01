aa6c697cfd25db4ac1299d9a266444697c84d0bf
user <email>
2025-09-01 23:03:39 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-//Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
 Route::get('/', function () {
